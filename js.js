503e72f1fd8347ffbc5accb6b1c1e003bba5cfb1
Suvonov2000 <suvonov200@gmail.com>
2024-10-16 13:02:29 +0900

    Palindrome

---
+++ js.js
@@ -0,0 +1,6 @@
+const isPalindrome = function isPalindrome(str) {
+  return str === str.split("").reverse().join("");
+};
+
+console.log(isPalindrome(`racecar`));
+console.log(isPalindrome(`hello`));
