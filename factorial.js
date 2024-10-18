d90ccab524217a4dc17e4e44f6724252cb277101
Suvonov2000 <suvonov200@gmail.com>
2024-10-18 17:17:28 +0900

    Factorial

--- factorial.js
+++ factorial.js
@@ -1,3 +1,10 @@
-function factorial(n) {}
-let s = 120;
+function factorial(n) {
+  let sum = 1;
+
+  for (let i = 1; i <= 5; i++) {
+    sum *= i;
+  }
+  return sum;
+}
+
 console.log(factorial(5));
