7364c1c4d52ccb21798bdb814ca06d2815c565bd
Suvonov2000 <suvonov200@gmail.com>
2024-10-20 04:08:55 +0900

    arrayInterSection

---
+++ ex3.js
@@ -0,0 +1,14 @@
+function arrayInterSection(arr1, arr2) {
+  return arr1.filter((value) => arr2.includes(value));
+}
+console.log(arrayInterSection([1, 2, 3], [2, 3, 4]));
+
+function intersection(arr1, arr2) {
+  return arr1.filter((value) => arr2.includes(value));
+}
+console.log(intersection([2, 3, 4], [3, 4, 5]));
+
+function intersction(arr1, arr2) {
+  return arr1.filter((value) => arr2.includes(value));
+}
+console.log(intersction([1, 2, 3], [2, 3, 4]));
