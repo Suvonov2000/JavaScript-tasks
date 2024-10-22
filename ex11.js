adfe375bc03ef6e4ea50d335eece73d688f01500
Suvonov2000 <suvonov200@gmail.com>
2024-10-22 18:37:59 +0900

    Class ex

---
+++ ex11.js
@@ -0,0 +1,10 @@
+class CreatPerson {
+  constructor(name, surname) {
+    this.name = name;
+    this.surname = surname;
+  }
+}
+
+const person1 = new CreatPerson("Asadbek", "Abduvoitov");
+
+console.log(person1);
