15249d52ccc9001f1521cb846401ed097e8540a2
Suvonov2000 <suvonov200@gmail.com>
2024-10-23 20:18:17 +0900

    class EXtends

---
+++ ex13.js
@@ -0,0 +1,30 @@
+class CreatPerson {
+  constructor(name, surname, number, currentCity) {
+    this.name = name;
+    this.surname = surname;
+    this.number = number;
+    this.currentCity = currentCity;
+  }
+  getFullName() {
+    return `${this.name} ${this.surname}`;
+  }
+}
+
+const person1 = new CreatPerson("Umidjon", "Suvonov", "090000000", "Tokyo");
+
+class Student extends CreatPerson {
+  constructor(name, surname, number, currentCity, comeCity) {
+    super(name, surname, number, currentCity);
+    this.comeCity = comeCity;
+  }
+}
+
+const worker = new Student(
+  "Tursun",
+  "Amriev",
+  "0700000000",
+  "Tokyo",
+  "Uzbekistan"
+);
+
+console.log(worker.getFullName());
