fda89d1554beb94d9f93ea2edb27b5a16eb5256b
Suvonov2000 <suvonov200@gmail.com>
2024-11-21 15:19:06 +0900

    async_await

---
+++ async_await.js
@@ -0,0 +1,9 @@
+const url = "https://api.sampleapis.com/coffee/hot";
+
+const getData = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+};
+
+getData();
